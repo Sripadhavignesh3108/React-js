c1372e9c77432df81fdd1500a125832b2519f479
Sripadhavignesh3108 <sripadhavignesh@gmail.com>
2024-08-08 14:14:00 +0530

    s sdsd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import CountrysMainPage from "./ReactTasks/country_DropList/CountrysMainPage";
+import axios from "axios";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchingData();
+  }, []);
+  async function fetchingData() {
+    const options = {
+      method: "GET",
+      url: "https://exercisedb.p.rapidapi.com/exercises",
+      params: {
+        limit: "70",
+        offset: "0",
+      },
+      headers: {
+        "x-rapidapi-key": "58419453fcmshebd9f6e41be0523p1feeeejsnca19c81374d8",
+        "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data);
+    } catch (error) {}
+  }
+  console.log(data);
   return (
     <>
-      <CountrysMainPage />
+      {data.map((each) => {
+        return <img src={each.gifUrl} />;
+      })}
     </>
   );
 }
